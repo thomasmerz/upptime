b63c02afb2fb2b29a582df563885b4b95aec4630
Thomas Merz <thomas.merz@dm.de>
2021-03-19 19:54:08 +0100

    🚸 improve user experience/usability

--- get_hist_from_gitlog.sh
+++ get_hist_from_gitlog.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+ALLSERVICES="$(ack name: .upptimerc.yml | grep -vE '^#|color-scheme|robots|Upptime'|awk {'print $3'}|xargs)"
+
 if [ -z "$1" ]; then
   echo -e "\nUsage:\033[1;33m\t`basename $0` [1-n] = count of how much occurrences ['service(s)']\n \033[0m"
+  echo -e "$(echo $ALLSERVICES|tr ' ' '\n')\n"
+  echo "ENTER to continue"; read -t 3; echo
 fi
 
 HEADCOUNT="${1:-1}"
-ALLSERVICES="$(ack name: .upptimerc.yml | grep -vE '^#|color-scheme|robots|Upptime'|awk {'print $3'}|xargs)"
 SERVICES="${2:-$ALLSERVICES}"
 
 for s in $SERVICES;
-  do git log|ack $s|head -$HEADCOUNT
+  do git log|ack "$s"|head -$HEADCOUNT
   [ "$HEADCOUNT" != 1 ] && { read -t 1; echo; }
   done
 
